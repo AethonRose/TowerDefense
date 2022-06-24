57e38ed386d72fdbfd6914d056acd44496871586
AethonRose <bobbysteiskal8565@gmail.com>
2022-06-24 03:37:36 -0700

    Revert "smooth enemy movement (117)"
    This reverts commit e558d7bcff22567e7dd9af2eb53233075e5c3b0a.

--- Assets/Scripts/EnemyMovementHandler.cs
+++ Assets/Scripts/EnemyMovementHandler.cs
@@ -4,41 +4,22 @@ using UnityEngine;
 
 public class EnemyMovementHandler : MonoBehaviour
 {
-    //Create list of waypoints for enemy in inspector
     [SerializeField] List<Waypoint> waypoints = new List<Waypoint>();
-    [SerializeField] [Range(0,5)] float speed = 1f;
+    [SerializeField] float coroutineWaitTime = 1f;
 
     void Start() 
     {
         Debug.Log("Start");
-        //Starts Coroutine
         StartCoroutine(FollowWaypoints());
         Debug.Log("Start Moving");
     }
 
     IEnumerator FollowWaypoints()
     {
-        //Loop through each waypoint in waypoints List
         foreach (Waypoint waypoint in waypoints)
         {
-            //Set current & new Positions
-            Vector3 currentPosition = transform.position;
-            Vector3 newPosition = waypoint.transform.position;
-            //Default movementPercent to 0 for Lerp in while loop
-            float movementPercent = 0f;
-
-            transform.LookAt(newPosition);
-            //execute while movementPercent < 1
-            while (movementPercent < 1f)
-            {
-                //update movementPercent with speed * Time.deltatime
-                movementPercent += (speed * Time.deltaTime);
-                //lineral interpolate enemy position from current to newPosition, using movementPercent to move enemy
-                transform.position = Vector3.Lerp(currentPosition, newPosition, movementPercent);
-
-                //waits untill Lerp is completed
-                yield return new WaitForEndOfFrame();
-            }
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(coroutineWaitTime);
         }
     }
 }
